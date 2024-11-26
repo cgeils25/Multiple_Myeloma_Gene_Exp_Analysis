24789380e3af17ea5841416bbfc04eb2f143036a
cgeils25 <cgeils@email.sc.edu>
2024-11-26 13:29:37 -0500

    actually tested and working now

--- get_data.R
+++ get_data.R
@@ -3,21 +3,17 @@ library(R.utils)
 
 data_dir = 'data/'
 
-if (!file.exists(data_dir)) 
-{
+if (!file.exists(data_dir)) {
   dir.create(data_dir)
 }
 
-if (!file.exists('data/GDS5826.soft')) 
-{
+if (!file.exists('data/GDS5826.soft')) {
     gse_path = getGEOfile(GEO = "GDS5826", destdir = data_dir, amount = "data")
 
     gunzip(gse_path)
-}
-else {
-    gse = getGEO(filename = 'data/GDS5826.soft')
-}
+} 
 
+gse = getGEO(filename = 'data/GDS5826.soft')
 
 # not used but this is how you get the metadata
 metadata = Meta(gse)
